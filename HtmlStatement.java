102ca9213bde57e7de37b22e0d3357f319739b91
Igor Marques <igorma@ufmg.br>
2020-10-14 20:39:22 -0300

    Commit 17

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -1,7 +1,24 @@
 import java.util.Enumeration;
 
 public class HtmlStatement extends Statement {
-   public String value(Customer aCustomer) {
+   public String value(Customer aCustomer, String[] msgs) {
+      Enumeration rentals = aCustomer.getRentals();
+      String result = msgs[0] + aCustomer.getName() + msgs[1];
+      while (rentals.hasMoreElements()) {
+         Rental each = (Rental) rentals.nextElement();
+         //show figures for each rental
+         result += msgs[2] + each.getMovie().getTitle() + msgs[3] + 
+         String.valueOf(each.getCharge()) + msgs[4];
+      }
+      //add footer lines
+      result += msgs[5] +
+      String.valueOf(aCustomer.getTotalCharge()) + msgs[6];
+      result += msgs[7] +
+      String.valueOf(aCustomer.getTotalFrequentRenterPoints()) + msgs[8];
+      return result;
+   }
+   
+/* public String value(Customer aCustomer) {
       Enumeration rentals = aCustomer.getRentals();
       String result = "<H1>Rentals for <EM>" + aCustomer.getName() +
       "</EM></H1><P>\n";
@@ -19,4 +36,5 @@ public class HtmlStatement extends Statement {
       "</EM> frequent renter points<P>";
       return result;
    }
+*/
 }
--- TextStatement.java
+++ TextStatement.java
@@ -1,7 +1,24 @@
 import java.util.Enumeration;
 
 public class TextStatement extends Statement {
-   public String value(Customer aCustomer) {
+   public String value(Customer aCustomer, String[] msgs) {
+      Enumeration rentals = aCustomer.getRentals();
+      String result = msgs[0] + aCustomer.getName() + msgs[1];
+      while (rentals.hasMoreElements()) {
+         Rental each = (Rental) rentals.nextElement();
+         //show figures for each rental
+         result += msgs[2] + each.getMovie().getTitle() + msgs[3] + 
+         String.valueOf(each.getCharge()) + msgs[4];
+      }
+      //add footer lines
+      result += msgs[5] +
+      String.valueOf(aCustomer.getTotalCharge()) + msgs[6];
+      result += msgs[7] +
+      String.valueOf(aCustomer.getTotalFrequentRenterPoints()) + msgs[8];
+      return result;
+   }
+   
+/*   public String value(Customer aCustomer) {
       Enumeration rentals = aCustomer.getRentals();
       String result = "Rental Record for " + aCustomer.getName() +
       "\n";
@@ -19,4 +36,5 @@ public class TextStatement extends Statement {
       " frequent renter points";
       return result;
    }
+*/
 }
