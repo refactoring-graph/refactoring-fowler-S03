624ab58de846045151d079cf7b03ddf946c06cb8
Igor Marques <igorma@ufmg.br>
2020-10-14 20:18:21 -0300

    Commit 15

--- Price.java
+++ Price.java
@@ -3,12 +3,7 @@ public abstract class Price {
    
    public abstract double getCharge(int daysRented);
    
-   public int getFrequentRenterPoints(int daysRented) {	   	
-      int frequentRenterPoints = 0;
-      frequentRenterPoints ++;
-      // add bonus for a two day new release rental
-      if ((getPriceCode() == Movie.NEW_RELEASE) &&
-         daysRented > 1) frequentRenterPoints ++;
-      return frequentRenterPoints;
+   public int getFrequentRenterPoints(int daysRented) {
+       return 1;
    }
 }
